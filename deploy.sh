1614d2f1607510eb9e6a086e6658977cbdbf58ee
Daouda Ndoye <daouda.ndoye25@gmail.com>
2019-11-26 00:36:40 +0100

    run commit

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,28 @@
 #                   Auteur : Daouda
 #                   Date : 25/11/2019
 #############################################################################
-
+# create 
+echo ""
 if [ "$1" = "--create" ] 
 then echo "-option de create"
+nbre_machine=1
+["$2" !=""] && nbre_machine=$2
+docker run -tid --name alpine alpine:latest
+echo "j'ai crée ${nbre_machine}"
+echo ""
 elif [ "$1" = "--drop" ]
 then echo "-option de drop"
+docker rm -f alpine
+echo ""
 elif [ "$1" = "--infos" ]
 then echo "options de infos"
+echo ""
 elif [ "$1" = "--start" ]   
 then echo "options start"
+docker ps -a
+echo ""
 elif [ "$1" = "--ansible" ]
+echo ""
 then echo "options ansible"
 else
 echo "
