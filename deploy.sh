6fbe1b491f61ea37e45674073bc04c5a16697cc5
Daouda Ndoye <daouda.ndoye25@gmail.com>
2019-11-26 00:05:57 +0100

    second commit

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,18 @@
 #                   Auteur : Daouda
 #                   Date : 25/11/2019
 #############################################################################
+
+if [ "$1" = "--create" ] 
+then echo "-option de create"
+elif [ "$1" = "--drop" ]
+then echo "-option de drop"
+elif [ "$1" = "--infos" ]
+then echo "options de infos"
+elif [ "$1" = "--start" ]   
+then echo "options start"
+elif [ "$1" = "--ansible" ]
+then echo "options ansible"
+else
 echo "
       -options:
                - --create : lancer des conteneurs
@@ -12,3 +24,4 @@ echo "
 	       - --infos : caractéristiques des conteneurs (ip , nom , user)
 	       - --start : demarage des conteneurs 
 	       - --ansible: déploiment arborescence ansible "
+	      fi
