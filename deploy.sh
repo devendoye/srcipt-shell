c158b5d49240d86b5a94c45bf868a4d92863f7e5
Daouda Ndoye <daouda.ndoye25@gmail.com>
2019-11-25 23:08:00 +0100

    first version

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##############################################################################
+#                   Script de deploiment 
+#                   Auteur : Daouda
+#                   Date : 25/11/2019
+#############################################################################
+echo "
+      -options:
+               - --create : lancer des conteneurs
+	       - --drop :  supprimer les conteneurs créer par le deploy.sh
+	       - --infos : caractéristiques des conteneurs (ip , nom , user)
+	       - --start : demarage des conteneurs 
+	       - --ansible: déploiment arborescence ansible "
